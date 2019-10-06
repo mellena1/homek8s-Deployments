9857d8c8db0b890765cc909a4ac403be7c075be1
Andrew Mellen <andrew_mellen@icloud.com>
2019-10-05 23:52:27 -0400

    Needs some updates for k8s 1.16.x

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # Install helm
+echo "Installing helm"
 cd bootstrap/helm
 ./install-helm.sh
 cd ../..
 
 # Add secret for sealed-secrets
+echo "Installing sealed-secrets"
 cd bootstrap/sealed-secrets
 ./add-key.sh
 cd ../..
 
 # Install flux
+echo "Installing flux"
 cd bootstrap/flux
 ./install-flux.sh
 cd ../..
 
 # Output Flux git deploy key and copy to keyboard
-FLUX_POD=$(kubectl get pods --namespace flux -l "app=flux,release=flux" -o jsonpath="{.items[0].metadata.name}")
-KEY=$(kubectl -n flux logs $FLUX_POD | grep identity.pub | cut -d '"' -f2)
+KEY=$(fluxctl identity --k8s-fwd-ns devops)
 echo $KEY
 echo $KEY | pbcopy
--- bootstrap/flux/install-flux.sh
+++ bootstrap/flux/install-flux.sh
@@ -2,11 +2,9 @@
 
 helm repo add fluxcd https://charts.fluxcd.io
 
-kubectl apply -f https://raw.githubusercontent.com/fluxcd/flux/helm-0.10.1/deploy-helm/flux-helm-release-crd.yaml
-
 helm upgrade flux -f values.yaml \
     --version 0.14.1 \
     --wait \
     --install \
     --namespace devops \
-    weaveworks/flux
+    fluxcd/flux
